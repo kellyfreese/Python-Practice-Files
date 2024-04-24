6435219cf080007871af2cc6ca16d2bd66025544
Kelly F <95229425+kellyraefoote@users.noreply.github.com>
2024-04-24 10:34:55 -0700

    More list practice

---
+++ More Lists Practice/main.py
@@ -0,0 +1,19 @@
+family = [
+    "Rob", "Maisy", "Milo", "Darin", "Sarah", "Jacob", "Adam", "Emma",
+    "Rachel", "Hannah", "Rick", "Chandra", "Everett", "Breccen", "Bob",
+    "Helen", "Jared", "Russell", "Ashely", "Kelly"
+]
+
+dinner_group = family[:4]
+print(dinner_group)
+
+drinks_group = family[16:]
+print(drinks_group)
+
+hike_group = family[10:14]
+print(hike_group)
+
+walk = family[6]
+print(walk)
+
+print(len(family))
